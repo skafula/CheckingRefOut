e83cbbe9f925509825fcca64a9723a276d873c66
unknown <gyorgy.horak@gmail.com>
2023-07-11 18:49:49 +0200

    First try to upload

---
+++ ForMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckingRefOut
+{
+    internal class ForMethods
+    {
+        public bool CheckingOut(out int paramOut)
+        {
+            paramOut = 10;
+            return true;
+        }
+
+        public bool CheckingRef(ref int paramRef)
+        {
+            paramRef = 30;
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using CheckingRefOut;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var instance1 = new ForMethods();
+
+        // with ref modifier i have to intialize the variable and it's value will be modified in the method
+        int refParam = 0;
+        instance1.CheckingRef(ref refParam);
+        Console.WriteLine(refParam);
+
+        // with out modifier it doesn't neccessery to initialize the variable before, it's possible int the method parameter line. 
+        // it helps to return 2 or more values (like bool with return and the value of the random).
+        var trueOrFalse = instance1.CheckingOut(out int random);
+        Console.WriteLine($"{trueOrFalse}, {random}");
+
+        // as i defined random before in the CheckingOut method's parameterlist the variable is alive and is able to work with (it's in the stack??)
+        random = 20;
+        Console.WriteLine($"{trueOrFalse}, {random}");
+
+        trueOrFalse = instance1.CheckingRef(ref random);
+        Console.WriteLine($"{trueOrFalse}, {random}");
+
+        // if i declare the variable it's still useable on the Method param's list without its type keyword.
+        int random1;
+        trueOrFalse = instance1.CheckingOut(out random1);
+        Console.WriteLine(random1);
+    }
+}
